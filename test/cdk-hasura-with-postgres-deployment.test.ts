5b9c80661abe7eebb73b806571e07b900a677240
Tunmise Ogunniyi <ogunniyitunmise@gmail.com>
2025-07-09 23:01:23 +0100

    cdk  init

---
+++ test/cdk-hasura-with-postgres-deployment.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkHasuraWithPostgresDeployment from '../lib/cdk-hasura-with-postgres-deployment-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-hasura-with-postgres-deployment-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkHasuraWithPostgresDeployment.CdkHasuraWithPostgresDeploymentStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
